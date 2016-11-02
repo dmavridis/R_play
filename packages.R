022e075987fd31b5adea7917fcc6db54a362ca17
Dimitrios Mavridis <dmavridis@outlook.com>
2016-11-02 08:16:52 +0000

    Diamonds almost finished
    Prior to starting Lesson 4

---
+++ packages.R
@@ -0,0 +1,18 @@
+# Data wrangling
+
+install.packages("tidyr")
+library(tidyr)
+?gather
+?spread
+?unite
+?seperate
+
+install.packages("dplyr")
+library(dplyr)
+?select
+?filter
+?arrange
+?mutate
+?summarise
+?group_by
+
